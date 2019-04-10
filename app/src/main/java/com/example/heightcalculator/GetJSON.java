8b097445c568e07416b82bff1637cc6ae715c4e1
Dahl <Fatalgnome@hotmail.dk>
2019-04-10 11:04:55 +0200

    Added map and basic UI

---
+++ app/src/main/java/com/example/heightcalculator/GetJSON.java
@@ -0,0 +1,6 @@
+package com.example.heightcalculator;
+
+public class GetJSON
+{
+    
+}
--- app/src/main/java/com/example/heightcalculator/MainActivity.java
+++ app/src/main/java/com/example/heightcalculator/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.heightcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.webkit.HttpAuthHandler;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+{
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private LatLng position = new LatLng(21, -30);
+    private String elevationBaseUrl = "https://maps.googleapis.com/maps/api/elevation/json?";
+    private String curLocation = "locations=" + new LatLng(position.latitude,position.longitude);
+    private String key = "&key=" + R.string.google_maps_key;
+    private String completeUrl = elevationBaseUrl + curLocation + key;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+
+        // Add a marker on your current position and move the camera
+        //TODO: Use current position
+        LatLng mapsPos = new LatLng(-31, 30);
+        mMap.addMarker(new MarkerOptions().position(mapsPos).title("Current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mapsPos));
+
+
+    }
+
 }
