18640c3e291caa3987bc67c0e59f45bd824c4dc8
Dahl <Fatalgnome@hotmail.dk>
2019-05-09 11:51:50 +0200

    It works now, but i cant update the UI

--- app/src/main/java/com/example/heightcalculator/GetJSON.java
+++ app/src/main/java/com/example/heightcalculator/GetJSON.java
@@ -1,6 +1,6 @@
 package com.example.heightcalculator;
 
-import android.os.AsyncTask;
+import android.content.Context;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -9,9 +9,7 @@ import com.google.android.gms.maps.model.LatLng;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.io.Console;
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -19,38 +17,33 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
-public class GetJSON extends AsyncTask<Void,Void,Void>
+public class GetJSON
 {
 
     private LatLng position;
-    private int height;
-    private Request request;
-
-    //Fix Memory leak
-
+    private double height;
     private TextView text;
+    private Context context;
 
-    public GetJSON(TextView text, LatLng position)
+    public GetJSON(Context context, TextView text, LatLng position)
     {
+        this.context = context;
         this.text = text;
         this.position = position;
     }
 
-    @Override
-    protected void onPreExecute()
+
+
+    public void StartClient()
     {
+        OkHttpClient client = new OkHttpClient();
         String elevationBaseUrl = "https://maps.googleapis.com/maps/api/elevation/json?";
         String curLocation = "locations=" + position.latitude + "," + position.longitude;
         String key = "&key=" + "AIzaSyDcUC_tIE-tFIV1HieNXe5j6C5vgy6q_yQ";
         String completeUrl = elevationBaseUrl + curLocation + key;
-        request = new Request.Builder().url(completeUrl).build();
-    }
-
-    @Override
-    protected Void doInBackground(Void... arg0)
-    {
-        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(completeUrl).build();
 
         client.newCall(request).enqueue(new Callback()
         {
@@ -58,10 +51,14 @@ public class GetJSON extends AsyncTask<Void,Void,Void>
             public void onResponse(Call call, Response response) throws IOException
             {
                 try {
-                    JSONObject jsonObject = new JSONObject(response.body().string());
-                    Log.d("debug",response.body().string());
+                    ResponseBody body = response.body();
+                    String bodyString = body.string();
+                    JSONObject jsonObject = new JSONObject(bodyString);
+                    Log.d("debug",bodyString);
                     JSONArray results = jsonObject.getJSONArray("results");
-                    height = (int)results.get(0);
+                    JSONObject elevation = (JSONObject)results.get(0);
+                    height = elevation.getDouble("elevation");
+                    Log.d("debug", ""+height);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -73,37 +70,22 @@ public class GetJSON extends AsyncTask<Void,Void,Void>
             {
                 e.printStackTrace();
             }
-
-
         });
-        return null;
-    }
-
-
-
-    @Override
-    protected void onPostExecute(Void aVoid)
-    {
-        if (isCancelled())
-        {
-            text.setText(R.string.failed);
-        }
-        else
-        {
-            text.setText(height);
-        }
 
     }
+    private void SetHeight(){
+        //context.runOnUiThread(HeightRun);
+    }
+    public Runnable HeightRun = new Runnable() {
+        @Override
+        public void run() {
+            text.setText("" + height);
+        }
+    };
 
-    /*@Override
-    protected void onProgressUpdate(Void... values) {
-        super.onProgressUpdate(values);
-    }*/
-
-
-    public LatLng getPosition() { return position;}
-    public void setPosition(LatLng position) { this.position = position; }
-
-    public int getHeight() { return height; }
-
+    public double getHeight() {
+        return height;
+    }
 }
+
+
--- app/src/main/java/com/example/heightcalculator/MainActivity.java
+++ app/src/main/java/com/example/heightcalculator/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.heightcalculator;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v7.app.AppCompatCallback;
 import android.view.View;
-import android.webkit.HttpAuthHandler;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -25,21 +21,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.Console;
+import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
 {
     private GoogleMap mMap;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
     private FusedLocationProviderClient fusedLocationClient;
     private LatLng curPos;
     private TextView text;
+    private Context context;
+    private GetJSON getJSON;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -53,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         {
             mapFragment.getMapAsync(this);
         }
+        context = this;
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
@@ -85,11 +78,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     public void getLocation(View view)
     {
-        //GetJSON task = new GetJSON(text, new LatLng(21, -21));
-        //task.execute();
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
-
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>()
                     {
@@ -107,6 +97,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curPos, 15));
                                 mMap.animateCamera(CameraUpdateFactory.zoomIn());
                                 mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+                                getJSON = new GetJSON(context, text, new LatLng(curPos.latitude, curPos.longitude));
+                                getJSON.StartClient();
+                                SetHeight();
                             }
                         }
                     });
@@ -117,6 +110,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    //TODO: Add Asynctask, use the GetJSON class. Check slides for more info (santa download example)
+    private void SetHeight()
+    {
+        this.runOnUiThread(getJSON.HeightRun);
+    }
 
 }
